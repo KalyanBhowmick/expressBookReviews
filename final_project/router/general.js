a397b44ea9fa3e23abb263b20598837ce4f18583
Kalyan Bhowmick <kbhowmick067@gmail.com>
2025-09-13 19:55:50 +0530

    General routes logic added

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,7 +24,7 @@ public_users.get("/", function (req, res) {
 public_users.get("/isbn/:isbn", function (req, res) {
   //Write your code here
   const isbn = req.params.isbn;
-  const book = books.find((b) => b.isbn === isbn);
+  const book = books[isbn];
   if (book) {
     return res.send(JSON.stringify(book));
   } else {
@@ -35,21 +35,40 @@ public_users.get("/isbn/:isbn", function (req, res) {
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
   const author = req.params.author;
-  const booksByAuthor = books.filter((b) => b.author === author);
-  if (booksByAuthor.length > 0) {
-    return res.send(JSON.stringify(booksByAuthor));
-  } else {
-    return res.status(404).json({ message: "No books found by this author" });
+
+  const keys = Object.keys(books);
+
+  const result = [];
+
+  for (const key of keys) {
+    const book = books[key];
+    if (book.author === author) {
+      result.push(book);
+    }
   }
+
+  if (result.length > 0) {
+    return res.send(JSON.stringify(result));
+  }
+  return res.status(404).json({ message: "No books found by this author" });
 });
 
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
   const title = req.params.title;
-  const booksByTitle = books.filter((b) => b.title === title);
-  if (booksByTitle.length > 0) {
-    return res.send(JSON.stringify(booksByTitle));
-  } else {
+
+  const keys = Object.keys(books);
+
+  const result = [];
+  for (const key of keys) {
+    const book = books[key];
+    if (book.title === title) {
+      result.push(book);
+    }
+
+    if (result.length > 0) {
+      return res.send(JSON.stringify(result));
+    }
     return res.status(404).json({ message: "No books found with this title" });
   }
 });
@@ -57,7 +76,9 @@ public_users.get("/title/:title", function (req, res) {
 //  Get book review
 public_users.get("/review/:isbn", function (req, res) {
   const isbn = req.params.isbn;
-  const book = books.find((b) => b.isbn === isbn);
+
+  const book = books[isbn];
+
   if (book) {
     return res.send(JSON.stringify(book.reviews));
   } else {
